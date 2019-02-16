6da8c98758dcf92683be8e3f6cc8ba96e6e16d5f
Gustav Sandström <gussan@omegapoint.se>
2019-02-16 20:57:01 +0100

    solved abba by adding removal if in stack logic

--- OpenKattis_Stack_Construction/computeOperations.py
+++ OpenKattis_Stack_Construction/computeOperations.py
@@ -1,9 +1,23 @@
 def computeOperations(displayMessage):
   print("about to process:" + displayMessage)
   charList = list(displayMessage)
-  adding =  len(charList)
-  printing = len(charList)
-  removing = len(charList)
+  stack = []
+  adding =  0
+  printing = 0
+  removing = 0
+  for char in charList:
+    if not stack or char not in stack:
+      stack.append(char)
+      adding += 1
+    elif char in stack:
+      while char != stack[-1]:
+        removing += 1
+        stack.pop()
+    printing += 1
+  removing += len(stack)
+  print("adding:" +  str(adding))
+  print("priting:" +  str(printing))
+  print("removing:" +  str(removing))
   numberOfOperations = adding + printing + removing
   return numberOfOperations
 
